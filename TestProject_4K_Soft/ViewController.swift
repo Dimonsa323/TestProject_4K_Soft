401e9dffcd69ec0b606221488f3a5ab35fd157eb
Dima Hubenia <dima96gubenya@gmail.com>
2023-04-05 18:41:01 +0300

    Initial Commit

---
+++ TestProject_4K_Soft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject_4K_Soft
+//
+//  Created by Дима Губеня on 05.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
